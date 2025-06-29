252af084ebda9f1f8323340612c3ead0e666235a
Soumyodip Sutradhar <soumyodipsutra@gmail.com>
2025-06-29 18:06:07 +0530

    controller work

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import { connection } from './postgres/postgres';
+import router from './view/routes';
 
 const app=express();
+
+app.use(router)
 const PORT=8000;
 
 app.listen(PORT,()=>{
--- server/view/routes.js
+++ server/view/routes.js
@@ -1,9 +1,7 @@
 import express from "express";
-
+import { getAllEmp } from "../controller/userController.js";
 const router=express.Router()
 
-router.get("getAll",()=>{
-    console.log("GET");
-});
+router.get("/getAll",getAllEmp);
 
 export default router;
