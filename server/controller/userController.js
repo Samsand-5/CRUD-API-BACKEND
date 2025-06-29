34c19eca7693fa4475b61b2acc01782f4bab90c7
Soumyodip Sutradhar <soumyodipsutra@gmail.com>
2025-06-29 23:29:29 +0530

    PUT method created successfully

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,5 +1,7 @@
+import { where } from "sequelize";
 import { UserModel } from "../postgres/postgres.js"
 
+//GET
 export const getAllEmp=async(req,res)=>{
     try {
         const users=await UserModel.findAll();
@@ -12,6 +14,7 @@ export const getAllEmp=async(req,res)=>{
     }
 }
 
+//POST
 export const addEmp=async(req,res)=>{
     const {name,email,designation,empId}=req.body;
     try {
@@ -26,10 +29,15 @@ export const addEmp=async(req,res)=>{
     }
 }
 
+//PUT
 export const updateEmp=async(req,res)=>{
     let empId=req.params.empId;
     try {
-        
+        const emp = await UserModel.update(res.body,{where:{empId}})
+        if(emp[0]==0){
+            return res.status(404).json({message:"not found"})
+        }
+        return res.status(200).json({message:"updated successfully"})
     } catch (error) {
         return res.status(500).json({"error":"Internal server error"})
     }
